09305de26d857f3c21a90ff130c7d1339d853cbe
vkhansen <42178039+vkhansen@users.noreply.github.com>
2026-03-20 21:39:39 +0700

    update to new versions

--- telegrambot/main.py
+++ telegrambot/main.py
@@ -2,6 +2,7 @@ import asyncio
 import os
 
 from aiogram import Bot, Dispatcher, types
+from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 
 from dotenv import find_dotenv, load_dotenv
@@ -21,7 +22,7 @@ from common.bot_cmds_list import private
 
 # ALLOWED_UPDATES = ['message', 'edited_message', 'callback_query']
 
-bot = Bot(token=os.getenv("TOKEN"), parse_mode=ParseMode.HTML)
+bot = Bot(token=os.getenv("TOKEN"), default=DefaultBotProperties(parse_mode=ParseMode.HTML))
 bot.my_admins_list = []
 
 dp = Dispatcher()
